7150aaad2a9f02980be11aafef49956a87b60201
Genki Miyauchi <genki.miyauchi@kcl.ac.uk>
2016-12-04 16:13:03 +0000

    Upload project files

---
+++ server/switch.rb
@@ -0,0 +1,15 @@
+#! /usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+require 'em-websocket'
+require 'serialport'
+
+port = "/dev/ttyACM0"
+
+sp = SerialPort.new(port, 9600, 8, 1, SerialPort::NONE)
+
+EventMachine::WebSocket.start(:host => "0.0.0.0",:port => 51234) do |ws|
+	ws.onmessage { |msg|
+		sp.write msg
+	}
+end
